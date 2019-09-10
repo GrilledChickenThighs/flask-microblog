6c3989896da6573ef0fdde299c30726e07f7071f
Paul D. Mendes <pauldmendes5@gmail.com>
2019-09-10 18:05:07 +0300

    Added user Registration and Login functtionality

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+server = Flask(__name__)
+server.config.from_object(Config)
+db = SQLAlchemy(server)
+migrate = Migrate(server, db)
+
+# Login Manager
+login = LoginManager(server)
+login.login_view = 'login'  # Redirects users to login if they try to view protected page
+
+
+
 
 from app import routes, models
